ec5919d205f68d8bacb4921dc5d033b8406c4bc9
Giulio Dalla Vecchia <giulio.dallavecchia@eas.it>
2024-01-19 17:23:06 +0100

    Added .cmake file

---
+++ stm32h7xx_common_hal_driver.cmake
@@ -0,0 +1,20 @@
+#Description: CMSIS Include For Cortex-M, ARMv8-M, ARMv8.1-M; user_visible: True
+include_guard(GLOBAL)
+message("stm32h7xx_common_hal_driver component is included.")
+
+file(GLOB HAL_SRC_FILES    
+    "${CMAKE_CURRENT_LIST_DIR}/Src/stm32h7xx_hal.c"
+    "${CMAKE_CURRENT_LIST_DIR}/Src/stm32h7xx_hal_cortex.c"
+    "${CMAKE_CURRENT_LIST_DIR}/Src/stm32h7xx_hal_rcc.c"
+    "${CMAKE_CURRENT_LIST_DIR}/Src/stm32h7xx_hal_gpio.c"
+)
+
+target_sources(${PROJECT_NAME} PRIVATE
+	${HAL_SRC_FILES}
+)
+
+target_include_directories(${PROJECT_NAME} PUBLIC
+    ${CMAKE_CURRENT_LIST_DIR}/Inc
+)
+
+
